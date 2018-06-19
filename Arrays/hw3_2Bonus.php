eff9d688ea1232a46769ceb5203038e881313b4d
YuraPopovych <yurapopovychw@gmail.com>
2018-06-20 01:43:13 +0300

    formatting

--- Arrays/hw3_2Bonus.php
+++ Arrays/hw3_2Bonus.php
@@ -5,9 +5,7 @@ You need to find out a unique value in array, you are given arrays:
 [ 1, 1, 1, 2, 1, 1 ] => 2
 [ 0, 0, 0.55, 0, 0 ] => 0.55
 [3,1,5,3,7,4,1,5,7] => 4
-
  */
-
 function uniqueValue($array)
 {
     /*$ar1 = [ 1, 1, 1, 2, 1, 1 ];
